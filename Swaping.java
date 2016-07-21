6c24431ca6d45829aacad6884df5d4888cb2e374
Sivaprakashsiva <sivaprakasam.mcse@ksrce.ac.in>
2016-07-21 02:52:31 -0700

    Swaping.java

---
+++ Swaping.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Swaping{
+public static void main(String []args)
+{
+Scanner s=new Scanner(System.in);
+String str=s.nextLine();
+char c=str.tocharArray();
+System.out.println(Arrays.toString(c));
+for(int i=c[0];i<c.length;i=i+2;){
+System.out.println(Arrays.toString(c[i]);
+}
+}
+}
