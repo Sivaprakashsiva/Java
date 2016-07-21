89be657e961df0258e2014c5457a8dc1e2cb39ef
Sivaprakashsiva <sivaprakasam.mcse@ksrce.ac.in>
2016-07-21 01:45:01 -0700

    StrToInt.java

---
+++ StrToInt.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class StrToInt{
+public static void main(String[]args){
+Scanner s=new Scanner(System.in);
+String st=s.nextline();
+int n=Integer.parseInt(st);
+Sytem.out.println(n);
+}
+}
