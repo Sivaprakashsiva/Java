7fe705e8a016a6d5c8d9df16b8f610b3cc4abb27
Sivaprakashsiva <sivaprakasam.mcse@ksrce.ac.in>
2016-07-20 22:13:44 -0700

    Reverse.java

---
+++ Reverse.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Fact{
+public static void main(String []args){
+Scanner s=new Scanner(System.in)
+String n=s.nextLine();
+StringBuffer bu=new StringBuffer(n);
+StringBuffer bu1=new StringBuffer(bu);
+System.out.println("reverse String is"+bu1.reverse());
+}
+}
