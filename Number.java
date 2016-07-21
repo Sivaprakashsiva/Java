9906eaff47cc22f8c9b7e3b68e4efbcead12810a
Sivaprakashsiva <sivaprakasam.mcse@ksrce.ac.in>
2016-07-20 22:00:12 -0700

    Number.java

---
+++ Number.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Number{
+public static void main(){
+Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+if(n<=0)
+{
+return 0;
+}
+else if(n>0){
+return n;
+}
+else if(n>1&&n==" "){
+return n;
+}
+}
+}
