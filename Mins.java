6c6be35828d933efe71e8deb05b1f43522617f3b
Sivaprakashsiva <sivaprakasam.mcse@ksrce.ac.in>
2016-07-22 01:18:05 -0700

    Mins.java

---
+++ Mins.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Mins{
+public static void main(String[] args) throws Exception {
+    long time = System.currentTimeMillis(); 
+    DateTime time1 = new DateTime(time);
+    DateTime time2 = new DateTime(time + 120_000);
+    Minutes minutes = Minutes.minutesBetween(time1, time2);
+    System.out.println(minutes.getMinutes());
+}
+}
+
